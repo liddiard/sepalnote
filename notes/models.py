d3f628585dbbf111cdd1c151c7d65ed22f6036ef
omniaura <omniaura5@gmail.com>
2014-09-06 13:08:13 -0700

    added notes app with filled models and skeleton views

---
+++ notes/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+
+class Note(models.Model):
+    user = models.ForeignKey('User')
+    parent = models.ForeignKey('self', null=True, blank=True)
+    position = models.PositiveIntegerField(default=0, db_index=True)
+        # order in which the note should be displayed relative to siblings
+    number = models.PositiveIntegerField(db_index=True)
+        # note id unique *to this user*. used for pretty URL access to notes.
+    text = models.TextField(blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    public = models.BooleanField(default=False)
+    expanded_in_minor_pane = models.BooleanField(default=False)
+    expanded_in_major_pane = models.BooleanField(default=True)
+    
+    class Meta:
+        unique_together = (('parent', 'position'), ('user', 'number'))
+        ordering = ['position'] 
+
+    def next_note_number(self):
+        return Note.objects.filter(user=self.user).order_by('-number')\
+                           .first().number + 1
+    
+    def immediate_children(self):
+        return Note.objects.filter(parent=self)
+
+    def save(self, *args, **kwargs):
+        if self.pk is None:
+            self.position = self.next_note_number()
+        super(UserProfile, self).save(*args, **kwargs)
+
+    def __unicode__(self):
+        return self.text
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField('User')
+    focused_note = models.ForeignKey('Note', null=True, blank=True)
+    spellcheck = models.BooleanField(default=True)
+    
+    def root_notes(self):
+        return Note.objects.filter(user=self.user, parent=None) 
+        
+    def __unicode__(self):
+        return self.user
---
+++ notes/views.py
@@ -0,0 +1,13 @@
+from django.views.generic.base import View, TemplateView
+
+
+class FrontView(TemplateView):
+    pass
+
+
+class NotesView(TemplateView):
+    pass
+
+
+# class SearchView(AuthenticatedAjaxView):
+#    pass
