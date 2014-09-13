f3e4cde1d5c96b5e5f0fb47c11e4b69ad50e7a50
omniaura <omniaura5@gmail.com>
2014-09-12 17:56:11 -0700

    registered models in admin

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from notes.models import Note, UserProfile
+
+
+class NoteAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Note, NoteAdmin)
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
