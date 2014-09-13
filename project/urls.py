05563079463dc2c93ded65c8a6103eb534eb5b26
omniaura <omniaura5@gmail.com>
2014-09-12 18:00:49 -0700

    another project rename import fix

--- project/urls.py
+++ project/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'repo.views.FrontView', name='front'),
+    url(r'^$', 'notes.views.FrontView', name='front'),
     url(r'^note/', 'notes.views.NotesView'),
-
     url(r'^admin/', include(admin.site.urls)),
 )
