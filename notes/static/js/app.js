30257a8c4ad3c4be0b2dd3d878db0c0310b5d10d
omniaura <omniaura5@gmail.com>
2014-11-22 14:16:23 -0800

    update notes through api

--- notes/static/js/app.js
+++ notes/static/js/app.js
@@ -3,11 +3,32 @@
 
     app.controller('NotesController', function($http, $scope) {
         var form = this;
-        define(function (require) {
-            var diff = require('deep-diff');
-        });
         this.major_tree = [];
         this.minor_tree = [];
+
+        this.noteFromPath = function(path) {
+            var note = form.major_tree;
+            for (var i = 0; i < path.length-1; i++) {
+                note = note[path[i]];
+            }
+            return note;
+        }
+
+        this.applyDiff = function(diff) {
+            for (var i = 0; i < diff.length; i++) {
+                if (diff[i].path[diff[i].path.length-1] === "$$hashKey")
+                    continue;
+                else if (diff[i].kind === 'E') {
+                    var note = form.noteFromPath(diff[i].path);
+                    $http({
+                        method: 'POST',
+                        url: '/api/note/update/',
+                        data: $.param({id: note.id, text: note.text}),
+                        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+                    });
+                }
+            }
+        }
 
         $http.get('/api/note/major-pane/').success(function(data){
             form.major_tree = data;
@@ -15,8 +36,10 @@
             /* listen for changes in notes */
             $scope.$watch(
                 function() { return form.major_tree },
-                function() {
-                    console.log('note changed');
+                function(newValue, oldValue) {
+                    var diff = DeepDiff(oldValue, newValue);
+                    if (diff)
+                        form.applyDiff(diff);
                 },
                 true // objectEquality
             );
