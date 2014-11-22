2b67bea703351c50a6da76d5358a38a81dad9d2c
omniaura <omniaura5@gmail.com>
2014-11-22 13:33:16 -0800

    detect changes in notes

--- notes/static/js/app.js
+++ notes/static/js/app.js
@@ -1,14 +1,25 @@
 (function(){
     var app = angular.module('notes', []);
 
-    app.controller('NotesController', function($http) {
+    app.controller('NotesController', function($http, $scope) {
         var form = this;
+        define(function (require) {
+            var diff = require('deep-diff');
+        });
         this.major_tree = [];
         this.minor_tree = [];
 
         $http.get('/api/note/major-pane/').success(function(data){
             form.major_tree = data;
+
+            /* listen for changes in notes */
+            $scope.$watch(
+                function() { return form.major_tree },
+                function() {
+                    console.log('note changed');
+                },
+                true // objectEquality
+            );
         });
     });
-
 })();
